8d93c83e832103605448846e20ddf63874f87df5
Mario Pareja <pareja.mario@gmail.com>
2015-07-07 03:31:58 -0400

    Initial eslint configuration.
    I painstakingly reviewed all of the rules available(*) and put together
    this configuration file. Surely, there will be changes.
    
    Available Rules: http://eslint.org/docs/rules/
    
    * I haven't yet reviewed the handful of ES6 rules.

---
+++ README.md
@@ -0,0 +1,21 @@
+# JavaScript Linting
+
+The baseline configuration for linting JavaScript files using [eslint](http://eslint.org/) is contained in this module. The explicitly specified settings in this file are mostly non-defaults values. In a few potentially controversial cases, a value matching the default has been explicitly specified so readers considering altering the value are aware that an explicit decision was made to use it.
+
+## Usage
+
+Install this module as a `devDependency` and reference it from your eslint configuration. Here's one way to install the module and reference it from a repo-local eslint YAML file:
+
+```
+npm install --save-dev https://github.com/360incentives/eslint-config-base
+echo 'extends: base' > .eslintrc
+```
+
+The base configuration is not optimized for linting tests. Instead of declaring globals  like `describe`, `it` and `before` in each test file, create an override `.eslintrc` file inside your `test` directory which is configured for the `mocha` test environment.
+
+```
+echo '
+env:
+  mocha: true
+' > ./test/.eslintrc
+```
---
+++ index.js
@@ -0,0 +1,54 @@
+module.exports = {
+  "rules": {
+
+    //// possible errors (http://eslint.org/docs/rules/#possible-errors)
+
+    "comma-dangle": [ 2, "never" ],
+    "no-cond-assign": [ 2, "except-parens" ],
+
+    /// best practices (http://eslint.org/docs/rules/#best-practices)
+
+    "block-scoped-var": 2,
+    "curly": [ 2, "all" ],
+    "no-multi-spaces": [ 2, { "exceptions": { "VariableDeclarator": true } } ],
+    "dot-location": [ 2, "property" ],
+    "guard-for-in": 2,
+    "no-else-return": 2,
+    "no-self-compare": 2,
+    "no-sequences": 2,
+    "no-throw-literal": 2,
+    "no-warning-comments": [ 1, { "terms": [ "todo", "fixme" ] } ],
+    "radix": 2,
+
+    /// strict mode (http://eslint.org/docs/rules/#strict-mode)
+
+    "strict": 0,
+
+    /// variables (http://eslint.org/docs/rules/#variables)
+
+    "no-use-before-define": [ 2, "nofunc" ],
+
+    /// node (http://eslint.org/docs/rules/#nodejs)
+
+    "handle-callback-err": 2,
+    "no-sync": 2,
+
+    /// stylistic issues (http://eslint.org/docs/rules/#stylistic-issues)
+
+    "brace-style": [ 2, "1tbs", { "allowSingleLine": true } ],
+    "comma-style": [ 2, "last" ],
+    "indent": [ 2, 2 ],
+    "linebreak-style": [ 2, "unix" ],
+    "no-lonely-if": 2,
+    "object-curly-spacing": [ 2, "always" ], // may be high-impact - consider changing
+    "quotes": [ 2, "single", "avoid-escape" ],
+    "semi": [ 2, "always" ],
+    "space-after-keywords": 2,
+    "space-before-blocks": 2,
+    "space-before-function-paren": [ 2, { "anonymous": "always", "named": "never" } ],
+    "no-bitwise": 2
+  },
+  "env": {
+    "node": true
+  }
+};
